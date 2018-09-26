1876f71acdda66e74c75f35ac370b9c7de9edc43
Jiri Vlasak <jirivlasak9@gmail.com>
2018-09-26 12:02:50 +0200

    refactorings

--- Loader.py
+++ Loader.py
@@ -4,13 +4,12 @@ import os
 def Load(rep, composerNames):
     composers = []
     os.chdir("data")
-    for dir in os.listdir():
+    for dir in composerNames:
         os.chdir(dir)
         with open (rep, "rb") as fp:
             data = pickle.load(fp)
         name = composerNames.index(dir)
         composers.append((name,data))
         os.chdir("..")
+    os.chdir("..")
     return composers
-
-print(Load("pitches",["bach","trecento"]))
