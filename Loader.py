201b6f2c01ed5df9b406286ad87c63c950421f51
Jiri Vlasak <jirivlasak9@gmail.com>
2018-09-30 14:21:00 +0200

    modified Loader for easier use, added new classifiers to jup notebool

--- Loader.py
+++ Loader.py
@@ -1,15 +1,49 @@
-import pickle
 import os
+import pickle
+import random
+import numpy as np
 
-def Load(rep, composerNames):
+
+def Load(dataset_type, composer_names, train_ratio=0.8, all=True, num_data=50):
+    """Load specified dataset from data folder
+
+    params:
+    dataset_type: string - string representation of dataset type
+                            (ie. "pitches")
+    composer_names : list[string] - names of composers
+    train_ratio: double - percentage of data reserved for training data
+    all: bool - if all exaples from each composer should be loaded
+    num_data: int - number of examples to load from each directory,
+    ignored if all = True
+
+    returns touple (x_train, y_train, x_test, y_test)"""
     composers = []
     os.chdir("data")
-    for dir in composerNames:
+    for dir in composer_names:
         os.chdir(dir)
-        with open (rep + ".dat", "rb") as fp:
+        with open(dataset_type + ".dat", "rb") as fp:
             data = pickle.load(fp)
-        name = composerNames.index(dir)
-        composers.append((name,data))
+        y = composer_names.index(dir)
+        if (all):
+            composers.append((y, data))
+        else:
+            composers.append((y, data[:num_data]))
         os.chdir("..")
+
     os.chdir("..")
-    return composers
+    split = int(len(composers[0][1]) * train_ratio)
+    training_data = []
+    test_data = []
+    for composer in composers:
+        y = composer[0]
+        training_data.extend([[x, y] for x in composer[1][:split]])
+        test_data.extend([[x, y] for x in composer[1][split:]])
+    random.shuffle(training_data)
+
+    x_train = [a[0] for a in training_data]
+    y_train = [a[1] for a in training_data]
+    x_test = [a[0] for a in test_data]
+    y_test = [a[1] for a in test_data]
+
+    return (np.array(x_train), np.array(y_train), np.array(x_test),
+            np.array(y_test))
--- makedata.py
+++ makedata.py
@@ -1,47 +1,51 @@
-from music21 import *
-import numpy as np
 import os
 import pickle
 import random
 
-def GetPitchCounts(pitches):
-  for pitch in pitches:
-    pitchCount[pitch.step]+= 1
-  values = np.array(list(pitchCount.values()),dtype="float")
-  values /= values.max()
-  return values
+import music21
+import numpy as np
 
-def serialize():  
-  os.chdir("data")
-  for composer in composers:
-    label = composerNames[composer[0]]
-    os.chdir(label)
-    with open("pitches.dat","wb+") as f:
-      pickle.dump(composer[1],f)
+
+def GetPitchCounts(pitches):
+    for pitch in pitches:
+        pitchCount[pitch.step] += 1
+    values = np.array(list(pitchCount.values()), dtype="float")
+    values /= values.max()
+    return values
+
+
+def serialize():
+    os.chdir("data")
+    for composer in composers:
+        label = composerNames[composer[0]]
+        os.chdir(label)
+        with open("pitches.dat", "wb+") as f:
+            pickle.dump(composer[1], f)
+        os.chdir("..")
     os.chdir("..")
-  os.chdir("..")
+
 
 numScores = 80
 composerNames = ["bach", "trecento"]
 
 pitchCount = {"C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "A": 0, "B": 0}
 
-scorePaths= [random.sample(corpus.getComposer(composer),numScores) 
-            for composer in composerNames]
+scorePaths = [
+    random.sample(music21.corpus.getComposer(composer), numScores)
+    for composer in composerNames
+]
 
-composers=[]
+composers = []
 composerPitches = []
-for j,composer in enumerate(scorePaths):
-  composerPitches = []
-  for score in composer:
-    score = corpus.parse(score)
-    key = score.analyze("key")
-    i = interval.Interval(key.tonic, note.Note('C').pitch)
-    score = score.transpose(i)
-    pitchCounts = GetPitchCounts(score.pitches)
-    composerPitches.append(pitchCounts)
-  composers.append((j,composerPitches))
+for j, composer in enumerate(scorePaths):
+    composerPitches = []
+    for score in composer:
+        score = music21.corpus.parse(score)
+        key = score.analyze("key")
+        i = music21.interval.Interval(key.tonic, music21.note.Note('C').pitch)
+        score = score.transpose(i)
+        pitchCounts = GetPitchCounts(score.pitches)
+        composerPitches.append(pitchCounts)
+    composers.append((j, composerPitches))
 
 serialize()
-
-
